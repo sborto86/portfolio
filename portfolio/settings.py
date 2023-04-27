81e8e8d318dc8221d905e1ea99fbc49aedc8bffa
sborto86 <sborto81@gmail.com>
2023-04-27 20:42:58 +0200

    fixed HOST settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jd3d%1fym=hcw-g4ua++ah7&lde+p*+_-l#cq&$+c51(-a^5$i
 DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS').split(' ')
+#CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS').split(' ')
 
 # Application definition
 
